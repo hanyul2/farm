ab0871343a21ffb5aa15e50198b7e0b1d16e8754
hanyul2 <121495467+hanyul2@users.noreply.github.com>
2023-06-03 17:35:08 +0900

    fghj

--- Assets/script/GONGPO/nunal_moving.cs
+++ Assets/script/GONGPO/nunal_moving.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class nunal_moving : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Sprite Left;
+    public Sprite Right;
     void Start()
     {
         
@@ -13,6 +15,13 @@ public class nunal_moving : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.mousePosition.x <= 5)
+        {
+            GetComponent<SpriteRenderer>().sprite = Left;
+        }
+        if (Input.mousePosition.x > 5)
+        {
+            GetComponent<SpriteRenderer>().sprite = Right;
+        }
     }
 }
